2435d4d292ca5ab241b8987bb8207f66589f4928
norehgomez <norehgomez25@gmail.com>
2024-11-26 03:16:58 +0800

    add js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Add any interactive functionality here (e.g., form validation).
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Message sent! Thank you for contacting.');
+  });
+  
