20f967645b25b8419df83e76e11259dfe06cdb49
norehgomez <norehgomez25@gmail.com>
2024-11-29 21:49:09 +0800

    fix format

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Add any interactive functionality here (e.g., form validation).
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     alert('Message sent! Thank you for contacting.');
